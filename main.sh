89f80ab5b772a6e1b155a6c1e47b40255ee4b293
Daniel Passarelli <dpassarelli@gmail.com>
2022-05-04 12:09:11 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get install -y gnupg software-properties-common curl ca-ce
 echo .
 echo .
 echo "### Seting up stable repo ###"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo .
 echo .
@@ -19,7 +19,7 @@ if [ $? -ne 0 ]; then
     yes | apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
     sudo service docker start
     docker ps > /dev/null 2>&1
-    if [ $? -ne 0]; then
+    if [ $? -ne 0 ]; then
         echo "### A problem ocurred while trying to check the docker service runnin."
         echo .
         echo . 
